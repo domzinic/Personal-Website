b660843f3380a60f3a2a114a49544ae063045416
Dominic Petrut <dompetrut@gmail.com>
2026-05-14 02:17:35 -0400

    fix: switch to google font provider to unblock build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 import { defineConfig, fontProviders } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: 'static',
@@ -15,29 +13,13 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
 
   fonts: [
-      {
-          provider: fontProviders.local(),
-          name: 'Atkinson',
-          cssVariable: '--font-atkinson',
-          fallbacks: ['sans-serif'],
-          options: {
-              variants: [
-                  {
-                      src: ['./src/assets/fonts/atkinson-regular.woff'],
-                      weight: 400,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-                  {
-                      src: ['./src/assets/fonts/atkinson-bold.woff'],
-                      weight: 700,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-              ],
-          },
-      },
-	],
+    {
+      provider: fontProviders.google(),
+      name: 'Atkinson Hyperlegible',
+      cssVariable: '--font-atkinson',
+      fallbacks: ['sans-serif'],
+    },
+  ],
 
   vite: {
     plugins: [tailwindcss()],
